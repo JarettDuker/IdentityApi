3614503b650dbe0079f755bb5baedcbc7c391ebf
JarettDuker <jarettduker@kycourts.net>
2025-07-01 23:38:11 -0400

    Almost got the login api working, keep troubleshooting

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,3 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
 [ApiController]
 [Route("api/[controller]")]
 public class AuthController : ControllerBase
--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -2,6 +2,5 @@ using Microsoft.AspNetCore.Identity;
 
 public class ApplicationUser : IdentityUser
 {
-    public string? Name { get; init; }
-    public string? Password { get; set; }
+
 }
